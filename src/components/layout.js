2f49c6597660d9e9571ceb369509b667934a3a34
cyberatlas <10457502+cyberatlas@users.noreply.github.com>
2020-02-25 00:26:25 -0600

    Finished tutorial

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,43 +6,83 @@
  */
 
 import React from "react"
+import Helmet from 'react-helmet'
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+// const Layout = ({ children }) => {
+//   const data = useStaticQuery(graphql`
+//     query SiteTitleQuery {
+//       site {
+//         siteMetadata {
+//           title
+//         }
+//       }
+//     }
+//   `)
+//
+//   return (
+//     <>
+//       <Header siteTitle={data.site.siteMetadata.title} />
+//       <div
+//         style={{
+//           margin: `0 auto`,
+//           maxWidth: 960,
+//           padding: `0 1.0875rem 1.45rem`,
+//         }}
+//       >
+//         <main>{children}</main>
+//         <footer>
+//           © {new Date().getFullYear()}, Built with
+//           {` `}
+//           <a href="https://www.gatsbyjs.org">Gatsby</a>
+//         </footer>
+//       </div>
+//     </>
+//   )
+// }
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
         }
       }
-    }
-  `)
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )}
+  />
+)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
---
+++ src/pages/new.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+// TODO figure out this syntax
+const NewPage = () => (
+  <Layout>
+    <h1> I'm a new page</h1>
+  </Layout>
+)
+
+// TODO what does this do
+export default NewPage
